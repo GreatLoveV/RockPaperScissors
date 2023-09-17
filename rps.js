4725883780ecc342b0b935df4cc163f5a977310f
pharma <92540444+GreatLoveV@users.noreply.github.com>
2023-09-17 18:44:54 +0100

    completed the exersise by making game() changed from ternary conditionals to normal conditionals as they were too restricting in terms of linking the two functions together didn't use loops as per the exersise and roadmap the game function prompts you for a choice after five rounds the function alerts the winner, there's no draw as its a 5 round game

--- rps.js
+++ rps.js
@@ -1,36 +1,93 @@
-// createthe opponent side's (computer) RPS choice function
+// create the opponent side's (computer) RPS choice function
 function getComputerChoice(){
     let choices = ["rock", "paper", "scissors"];
     return choices[Math.floor(Math.random() * 3)];
 }
 // RPS FUNCTION
-function playRound(playerSelection , computerSelection){
-
-    //fetch the computer's random choice   
-    computerSelection = getComputerChoice();
+function playRound(playerSelection , computerSelection){  
     //convert to lower case for case insensitivity
     playerSelection = playerSelection.toLowerCase();
-    // setting the results using ternary conditionals
     
-    return( //used the parentheses next to return as you need them if you want to return several lines like below
+    if( //added the winning conditions
         (playerSelection === "rock" && computerSelection === "scissors") ||
         (playerSelection === "paper" && computerSelection === "rock")    ||
         (playerSelection === "scissors" && computerSelection === "paper")
-    )
-        //added the winningconditions
-        ? `You Win! ${playerSelection} beats ${computerSelection}`
-        : (
-            (playerSelection === "scissors" && computerSelection === "rock") ||
-            (playerSelection === "rock" && computerSelection === "paper")    ||
-            (playerSelection === "paper" && computerSelection === "scissors")
-        )
-        //added the losing conditions
-        ? `You Lose! ${computerSelection} beats ${playerSelection}`
-        //if neither are true then its a draw 
-        :`It's a Draw! Both chose ${computerSelection}`;  
+    ) {
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    } else if(//added the losing conditions
+        (playerSelection === "scissors" && computerSelection === "rock") ||
+        (playerSelection === "rock" && computerSelection === "paper")    ||
+        (playerSelection === "paper" && computerSelection === "scissors")
+    ){
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+        
+    }else{//if neither are true then its a draw 
+        return`It's a Draw! Both chose ${playerSelection}`
+        
+    }
 }
-console.log(playRound("scissors"))
 
+
+
+function game() {
+    let scorePlayer = 0;
+    let scoreComputer = 0;
+    playerSelection = prompt("Rock Papers or Scissors?");
+    computerSelection = getComputerChoice();
+    result = playRound(playerSelection, computerSelection)
+    playRound(playerSelection)
+    if (result.includes('Win')) scorePlayer++;
+    else if (result.includes('Lose'))   scoreComputer++;
+    console.log(result)
+
+    
+    playerSelection = prompt("Rock Papers or Scissors?");
+    computerSelection = getComputerChoice();
+    result = playRound(playerSelection, computerSelection)
+    playRound(playerSelection)
+    if (result.includes('Win')) scorePlayer++
+    else  if (result.includes('Lose')) scoreComputer++
+    console.log(result)
+
+
+    playerSelection = prompt("Rock Papers or Scissors?");
+    computerSelection = getComputerChoice();
+    result = playRound(playerSelection, computerSelection) 
+    playRound(playerSelection)
+    if (result.includes('Win')) scorePlayer++;
+    else if (result.includes('Lose'))  scoreComputer++;
+    console.log(result)
+
+
+    playerSelection = prompt("Rock Papers or Scissors?");
+    computerSelection = getComputerChoice();
+    result = playRound(playerSelection, computerSelection)
+    playRound(playerSelection)
+    if (result.includes('Win')) scorePlayer++;
+    else if (result.includes('Lose')) scoreComputer++;
+    console.log(result)
+
+
+    playerSelection = prompt("Rock Papers or Scissors?");
+    computerSelection = getComputerChoice();
+    result = playRound(playerSelection, computerSelection)
+    playRound(playerSelection)
+    if (result.includes('Win')) scorePlayer++;
+    else if (result.includes('Lose')) scoreComputer++;
+    console.log(result)
+
+    if (scorePlayer > scoreComputer) {
+        alert("You Win, you scored more than mr computer")
+    }else{
+        alert("Womp Womp, You lost broke aah")
+    }
+
+}
+game();
+//     (scorePlayer > scoreComputer)? alert(`You Win! You scored ${scorePlayer} out of 5`)
+//     :alert(`You Lost! You only scores ${scorePlayer} out of 5`);
+// }
+// game()
 
 
 
