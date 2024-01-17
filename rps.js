b7192a78166959f8923da29d83e1b2b937cd3522
pharma <92540444+GreatLoveV@users.noreply.github.com>
2024-01-17 14:32:37 +0100

    Simplified button eventlisteners by adding a for each loop for all three buttons

--- rps.js
+++ rps.js
@@ -1,61 +1,111 @@
 // create the opponent side's (computer) RPS choice function
-function getComputerChoice(){
-    let choices = ["rock", "paper", "scissors"];
-    return choices[Math.floor(Math.random() * 3)];
+function getComputerChoice() {
+  let choices = ["rock", "paper", "scissors"];
+  return choices[Math.floor(Math.random() * 3)];
 }
+let scorePlayer = 0;
+let scoreComputer = 0;
+let roundCount = 0;
 // RPS FUNCTION
-function playRound(playerSelection , computerSelection){  
-    //convert to lower case for case insensitivity
-    playerSelection = playerSelection.toLowerCase();
-    const resultDisplay = document.querySelector('#result')
-    
-    if( //added the winning conditions
-        (playerSelection === "rock" && computerSelection === "scissors") ||
-        (playerSelection === "paper" && computerSelection === "rock")    ||
-        (playerSelection === "scissors" && computerSelection === "paper")
-    ) {
-        resultDisplay.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
-    } else if(//added the losing conditions
-        (playerSelection === "scissors" && computerSelection === "rock") ||
-        (playerSelection === "rock" && computerSelection === "paper")    ||
-        (playerSelection === "paper" && computerSelection === "scissors")
-    ){
-        resultDisplay.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
-    
-    }else{//if neither are true then its a draw 
-        resultDisplay.textContent = `It's a Draw! Both chose ${playerSelection}`;
-        
-    }
-}
-// event listeners
-const rck = document.querySelector('#rock');
-rck.addEventListener('click', () => {
-    let computerSelection = getComputerChoice();
-    playRound('rock', computerSelection);
-});
-const pap = document.querySelector('#paper');
-pap.addEventListener('click', () => {
-    let computerSelection = getComputerChoice();
-    playRound('paper', computerSelection);
-});
-const sci = document.querySelector('#scissors');
-sci.addEventListener('click', () => {
-    let computerSelection = getComputerChoice();
-    playRound('scissors', computerSelection);
-});
+const resultDisplay = document.querySelector("#result");
+function playRound(playerSelection, computerSelection) {
+  //convert to lower case for case insensitivity
+  playerSelection = playerSelection.toLowerCase();
+  
 
+  if (
+    //added the winning conditions
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    resultDisplay.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
+    scorePlayer++;
+    roundCount++;
+  } else if (
+    //added the losing conditions
+    (playerSelection === "scissors" && computerSelection === "rock") ||
+    (playerSelection === "rock" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "scissors")
+  ) {
+    resultDisplay.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
+    scoreComputer++;
+    roundCount++;
+  } else {
+    //if neither are true then its a draw
+    resultDisplay.textContent = `It's a Draw! Both chose ${playerSelection}`;
+    roundCount++;
+  }
+}
+const btn = document.querySelectorAll(".btns");
+console.log(btn);
+
+// event listeners
+const scoreDisplay = document.querySelector("#score");
+const rck = document.querySelector("#rock");
+rck.addEventListener("click", () => {
+  let computerSelection = getComputerChoice();
+  playRound("rock", computerSelection);
+  //     scoreDisplay.textContent = `Score is now ${scorePlayer} to ${scoreComputer}`
+  // }else if (roundCount===5 && scorePlayer > scorePlayer ){
+  //     scoreDisplay.textContent = `You win by ${scorePlayer} to ${scoreComputer}`
+  // }else if (roundCount===5 && scorePlayer < scorePlayer ) {
+  //     scoreDisplay.textContent = `You lose by ${scorePlayer} to ${scoreComputer}`
+  // }else {
+  //     scoreDisplay.textContent = `Its a tie, ${scorePlayer} to ${scoreComputer}`
+  // }
+});
+const pap = document.querySelector("#paper");
+pap.addEventListener("click", () => {
+  let computerSelection = getComputerChoice();
+  playRound("paper", computerSelection);
+  //     scoreDisplay.textContent = `Score is now ${scorePlayer} to ${scoreComputer}`
+  // }else if (roundCount===5 && scorePlayer > scoreComputer ){
+  //     scoreDisplay.textContent = `You win by ${scorePlayer} to ${scoreComputer}`
+  // }else if (roundCount===5 && scorePlayer < scoreComputer ) {
+  //     scoreDisplay.textContent = `You lose by ${scorePlayer} to ${scoreComputer}`
+  // }else {
+  //     scoreDisplay.textContent = `Its a tie, ${scorePlayer} to ${scoreComputer}`
+  // }
+});
+const sci = document.querySelector("#scissors");
+sci.addEventListener("click", () => {
+  let computerSelection = getComputerChoice();
+  playRound("scissors", computerSelection);
+  //     scoreDisplay.textContent = `Score is now ${scorePlayer} to ${scoreComputer}`
+  // }else if (roundCount===5 && scorePlayer > scoreComputer){
+  //     scoreDisplay.textContent = `You win by ${scorePlayer} to ${scoreComputer}`
+  // }else if (roundCount===5 && scorePlayer < scoreComputer ) {
+  //     scoreDisplay.textContent = `You lose by ${scorePlayer} to ${scoreComputer}`
+  // }else{
+  //     scoreDisplay.textContent = `Its a tie, ${scorePlayer} to ${scoreComputer}`
+  // }
+});
+btn.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (roundCount < 5) {
+      scoreDisplay.textContent = `Score is now ${scorePlayer} to ${scoreComputer}`;
+    } else if (roundCount === 5 && scorePlayer > scoreComputer) {
+      scoreDisplay.textContent = `You win by ${scorePlayer} to ${scoreComputer}`;
+    } else if (roundCount === 5 && scorePlayer < scoreComputer) {
+      scoreDisplay.textContent = `You lose by ${scorePlayer} to ${scoreComputer}`;
+    } else if (roundCount === 5 && scorePlayer === scoreComputer) {
+      scoreDisplay.textContent = `Its a tie, ${scorePlayer} to ${scoreComputer}`;
+    } else{
+        scoreDisplay.textContent = 'baraka 3lik'
+        resultDisplay.textContent = ``;
+    }
+  });
+});
 // function game() {
 //     let scorePlayer = 0;
 //     let scoreComputer = 0;
-//     playerSelection = prompt("rock paper or scissors?");
-//     computerSelection = getComputerChoice();
 //     result = playRound(playerSelection, computerSelection)
 //     playRound(playerSelection)
 //     if (result.includes('Win')) scorePlayer++;
 //     else if (result.includes('Lose'))   scoreComputer++;
 //     console.log(result)
 
-    
 //     playerSelection = prompt("rock paper or scissors?");
 //     computerSelection = getComputerChoice();
 //     result = playRound(playerSelection, computerSelection)
@@ -64,15 +114,13 @@ sci.addEventListener('click', () => {
 //     else  if (result.includes('Lose')) scoreComputer++
 //     console.log(result)
 
-
 //     playerSelection = prompt("rock paper or scissors?");
 //     computerSelection = getComputerChoice();
-//     result = playRound(playerSelection, computerSelection) 
+//     result = playRound(playerSelection, computerSelection)
 //     playRound(playerSelection)
 //     if (result.includes('Win')) scorePlayer++;
 //     else if (result.includes('Lose'))  scoreComputer++;
 //     console.log(result)
-
 
 //     playerSelection = prompt("rock paper or scissors?");
 //     computerSelection = getComputerChoice();
@@ -81,7 +129,6 @@ sci.addEventListener('click', () => {
 //     if (result.includes('Win')) scorePlayer++;
 //     else if (result.includes('Lose')) scoreComputer++;
 //     console.log(result)
-
 
 //     playerSelection = prompt("rock paper or scissors?");
 //     computerSelection = getComputerChoice();
